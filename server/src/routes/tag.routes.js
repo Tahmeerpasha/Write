b7c77ff663319a4af2323047db64fcef2d165a51
Tahmeerpasha <tahmeerpashacjc@gmail.com>
2024-05-03 20:00:11 +0530

    Add: Protected routes

--- server/src/routes/tag.routes.js
+++ server/src/routes/tag.routes.js
@@ -1,12 +1,13 @@
 import Router from 'express';
 import { createTag, deleteTag, getTagById, getTags, updateTag } from '../controllers/tag.controller.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
-router.post('/', createTag);
+router.post('/', verifyJWT, createTag);
 router.get('/', getTags);
 router.get('/:id', getTagById);
-router.put('/:id', updateTag);
-router.delete('/:id', deleteTag);
+router.put('/:id', verifyJWT, updateTag);
+router.delete('/:id', verifyJWT, deleteTag);
 
 export default router;
